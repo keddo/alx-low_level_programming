5c11fe3134657e8846e6077687e66b1202bcb403
keddo <adu.kedir.abdurahman@gmail.com>
2023-10-12 03:15:30 +0300

    Write a function that searches for an integer.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 
-    if (array == NULL || action == NULL)
-        return;
+if (array == NULL || action == NULL)
+return;
 
-    while (size-- > 0)
-    {
-        action(*array);
-        array++;
-    }
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 }
