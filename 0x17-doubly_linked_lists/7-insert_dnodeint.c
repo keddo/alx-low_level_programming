3e4f1f4d24424997f2f3f323859083fbab63fb2c
keddo <adu.kedir.abdurahman@gmail.com>
2024-02-17 23:06:08 +0300

    Insert node at given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to head of list
- * @idx: index of the list where the new node should be added
- * @n: data of the new node
+ * insert_dnodeint_at_index - inserts node at given index
+ * @h: address of pointer to current head node
+ * @idx: the index to at which to insert
+ * @n: the value of the inserted node
  *
- * Return:  the address of the new node, or NULL if it failed
+ * Return: new node or NULL
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+
+if (!h || !new)
+return (new ? free(new), NULL : NULL);
+node = *h;
+new->n = n;
+if (!idx)
+{
+new->prev = NULL;
+new->next = node ? node : NULL;
+if (node)
+node->prev = new;
+return (*h = new);
+}
+for (; node; node = node->next, idx--)
+{
+if (idx - 1 == 0)
+{
+new->prev = node;
+new->next = node->next;
+if (new->next)
+new->next->prev = new;
+node->next = new;
+return (new);
+}
+}
+return (free(new), NULL);
 }
